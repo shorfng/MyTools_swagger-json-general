56c0b32cb0384a79f3655630bb97b7e056de1905
蓝田_Loto <shorfng@126.com>
2022-01-10 19:31:46 +0800

    Initial commit

---
+++ src/main/java/com/loto/SwaggerApplication.java
@@ -0,0 +1,9 @@
+package com.loto;
+
+import com.loto.code.build.TemplateBuilder;
+
+public class SwaggerApplication {
+    public static void main(String[] args) {
+        TemplateBuilder.builder();
+    }
+}
